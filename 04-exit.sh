00f1c97edea9ae8981ff363b8fb9d24b851d81a9
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-05-01 13:37:05 -0700

    created shell script for frontend

--- 04-exit.sh
+++ 04-exit.sh
@@ -1,3 +1,23 @@
+sample2() {
+  echo Hello1
+  return
+  echo Hello2
+ }
+
+
+
 echo Hello1
 exit
 echo Hello2
+# Exit the script using exit command
+# Exit the function using return command to main program
+
+# Both commands behaviours is same and it is exiting
+# while exiting both the commands can hold some information and it can show that information
+# That information is called the status. Aslo called as Exit Status
+# That information is a number from 0-255
+# Zero considers as success message
+# Non Zero considers as failure message
+
+# we can check this information using a variable which is? Meaning $? can show exit status of return or exit command.
+
